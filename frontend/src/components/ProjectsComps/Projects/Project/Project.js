7f55f1e0c7fe64fb60e9f5333cffdec2feb553a3
Tankiac <tarikov.mail@gmail.com>
2021-08-31 18:35:25 +0200

    made projects page responsive

--- frontend/src/components/ProjectsComps/Projects/Project/Project.js
+++ frontend/src/components/ProjectsComps/Projects/Project/Project.js
@@ -1,22 +1,55 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import classes from "./Project.module.css"
 
 const Project = (props) => {
+    const screenSize = useSelector(state => state.screenSize);
+
     const openNewTab = (url) => {
         window.open(url,'_blank');
     }
 
     return (
-                <div className={classes.Project}>
-                    <div className={classes.ProjectImgContainer}>
+                <div className={
+                    `${classes.Project} ${screenSize === "large" ? classes.ProjectLarge :
+                    screenSize === "medium" ? classes.ProjectMedium : 
+                    screenSize === "small" ? classes.ProjectSmall : 
+                    screenSize === "extraSmall" ? classes.ProjectExtraSmall : 
+                    null}`
+                }>
+                    <div className={
+                        `${classes.ProjectImgContainer} ${screenSize === "large" ? classes.ProjectImgContainerLarge :
+                        screenSize === "medium" ? classes.ProjectImgContainerMedium : 
+                        screenSize === "small" ? classes.ProjectImgContainerSmall : 
+                        screenSize === "extraSmall" ? classes.ProjectImgContainerExtraSmall : 
+                        null}`
+                    }>
                         <img className={classes.ProjectImg} src={props.imgURL}/>
                     </div>
-                    <div className={classes.ProjectDesc}>
-                        <div className={classes.ProjectTitle}>{props.name}</div>
+                    <div className={
+                        `${classes.ProjectDesc} ${screenSize === "large" ? classes.ProjectDescLarge :
+                        screenSize === "medium" ? classes.ProjectDescMedium : 
+                        screenSize === "small" ? classes.ProjectDescSmall : 
+                        screenSize === "extraSmall" ? classes.ProjectDescExtraSmall : 
+                        null}`
+                    }>
+                        <div className={
+                            `${classes.ProjectTitle} ${screenSize === "large" ? classes.ProjectTitleLarge :
+                            screenSize === "medium" ? classes.ProjectTitleMedium : 
+                            screenSize === "small" ? classes.ProjectTitleSmall : 
+                            screenSize === "extraSmall" ? classes.ProjectTitleExtraSmall : 
+                            null}`
+                        }>{props.name}</div>
                         <div className={classes.ProjectDescText}>{props.desc}</div>
                             <button 
-                                className={classes.VisitSiteButton}
+                                className={
+                                    `${classes.VisitSiteButton} ${screenSize === "large" ? classes.VisitSiteButtonLarge :
+                                    screenSize === "medium" ? classes.VisitSiteButtonMedium : 
+                                    screenSize === "small" ? classes.VisitSiteButtonSmall : 
+                                    screenSize === "extraSmall" ? classes.VisitSiteButtonExtraSmall : 
+                                    null}`
+                                }
                                 onClick={() => openNewTab(props.link)}>
                                     Visit Site
                                 </button>
--- frontend/src/pages/ProjectPage/ProjectPage.js
+++ frontend/src/pages/ProjectPage/ProjectPage.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import classes from "./ProjectPage.module.css"
 import Projects from "../../components/ProjectsComps/Projects/Projects";
 
 const ProjectPage = (props) => {
+    const screenSize = useSelector(state => state.screenSize);
+
     return (
                 <div className={classes.ProjectPage}>
-                    <div className={classes.ProjectsTitle}>
+                    <div className={
+                        `${classes.ProjectsTitle} ${screenSize === "large" ? classes.ProjectsTitleLarge :
+                        screenSize === "medium" ? classes.ProjectsTitleMedium : 
+                        screenSize === "small" ? classes.ProjectsTitleSmall : 
+                        screenSize === "extraSmall" ? classes.ProjectsTitleExtraSmall : 
+                        null}`
+                    }>
                         MY PROJECTS
                     </div>
                     <Projects/>
