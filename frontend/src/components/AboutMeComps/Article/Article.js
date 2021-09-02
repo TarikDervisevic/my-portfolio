13923fe95ec2f7d0f288f6b0cbd013adfa703510
Tankiac <tarikov.mail@gmail.com>
2021-09-02 21:27:52 +0200

    added CV draft

--- frontend/src/components/AboutMeComps/Article/Article.js
+++ frontend/src/components/AboutMeComps/Article/Article.js
@@ -14,7 +14,40 @@ const Article = (props) => {
                     screenSize === "extraSmall" ? classes.ArticleExtraSmall : 
                     null}`
                 }>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                    <h2 className={classes.Title}>Profile</h2>
+                        <div>An enthusiastic young full-stack web developer with a passion for code. Quick to pick up new languages/technologies and enjoys developing efficient solutions to challenging problems.</div>
+
+                    <h2 className={classes.Title}>Programming skills</h2>
+                        <div><span className={classes.Bold}>Frontend -</span> HTML/CSS, Bootstrap, React, jQuery</div>
+                        <div><span className={classes.Bold}>Backend -</span> Node.js/Express.js</div>
+                        <div><span className={classes.Bold}>Databases -</span> MongoDB</div>
+                        <div><span className={classes.Bold}>Version Control System -</span> Git</div>
+
+                    <h2 className={classes.Title}>Projects</h2>
+                        <div className={classes.Bold}>PixelStore</div>
+                        <div>An online store made with React and Node/Express</div>
+                        <a>www.pixelstorelink.com</a>
+                        <div className={classes.Bold}>SquareClicker</div>
+                        <div>A simple game made with React</div>
+                        <a>www.squareclickerlink.com</a>
+
+                    <h2 className={classes.Title}>Education</h2>
+                        <div><span>Medical nurse</span> | <span>"Medicinska škola Bihać"</span></div>
+                        <div>2016 - 2021</div>
+
+                    <h2 className={classes.Title}>Languages</h2>
+                        <div>
+                            <div className={classes.Bold}>Bosnian/Croatian/Serbian</div>
+                            <div className={classes.Bold}>English</div>
+                            <div className={classes.Bold}>German</div>
+                        </div>
+                        <div>
+                            <div>Native</div>
+                            <div>Fluent</div>
+                            <div>Good</div>
+                        </div>
+                        
                 </div>
     )
 }
