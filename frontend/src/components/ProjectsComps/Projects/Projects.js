a18bb317e3b5ea1f52878b7d181a033819eaeb03
Tarik Dervišević <tarikov.mail@gmail.com>
2021-11-18 20:59:39 +0100

    fixed link for pixelstore

--- frontend/src/components/ProjectsComps/Projects/Projects.js
+++ frontend/src/components/ProjectsComps/Projects/Projects.js
@@ -5,7 +5,7 @@ import Project from "./Project/Project";
 
 const Projects = (props) => {
     const projectList = [
-        {name: "PixelStore", desc: "A responsive online store made with the MERN stack", link: "https://salty-eyrie-09068.herokuapp.com", imgURL: "https://i.imgur.com/TW7A8T2.png"},
+        {name: "PixelStore", desc: "A responsive online store made with the MERN stack", link: "https://pixelstore-loads-slowly.herokuapp.com/", imgURL: "https://i.imgur.com/TW7A8T2.png"},
         {name: "SquareClicker", desc: "My first React project, a simple game", link: "https://squareclicker.web.app", imgURL: "https://i.imgur.com/1Ggwv5C.png"}
     ]
 
